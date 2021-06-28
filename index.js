0706b7b2dd61b419316a05372f400343b26e6bf4
Luzanne <luzannejss@gmail.com>
2021-06-28 18:12:08 +0200

    Updated Javascript
    Updated:
    Time
    Populating HTML

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
@@ -6,7 +30,7 @@ function searchCity(event) {
   let apiKey = "4ad046b76be74520a0cad560d5d143a8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
 
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature).then(formatDate);
 }
 let cityInput = document.querySelector(".searchBar");
 cityInput.addEventListener("submit", searchCity);
@@ -15,4 +39,10 @@ function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector(".temperature");
   temperatureElement.innerHTML = `${temperature}°C`;
+  let descriptionElement = document.querySelector(".description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector(".date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
