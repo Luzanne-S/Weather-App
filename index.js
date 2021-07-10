7ba588433e092d09edd1322588d622c5aa277643
Luzanne <luzannejss@gmail.com>
2021-07-10 09:52:25 +0200

    Updated Javascript

--- index.js
+++ index.js
@@ -68,7 +68,6 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
-  getForecast(response.data.coord);
 }
 
 //city//
@@ -140,3 +139,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 
 searchCity("Johannesburg");
+getForecast(response.data.coord);
